c28502eb145d8cf777fb99ab60bcac3e19f9027a
Caleb Kussmaul <cwkussmaul@wpi.edu>
2017-04-12 14:07:10 -0400

    updated to newer version of healthscore.js

--- static/js/healthscore.js
+++ static/js/healthscore.js
@@ -1,51 +1,53 @@
 var canvases = [];
 var healths = [];
 var dispHealths = [];
+var showTexts = [];
+var borderWidths = [];
 
 var scores = 0;
 
 var rate = .03;
-var borderWidth = 3;
 var borderRadius = 8;
 var fontName = 'helvetica';
 var mercuryColor = '#C1272D';
 var glassColor = '#E6E6E6';
 
-function createHealthscore(healthScore, width, height) {
-  var score = scores;
+function createHealthscore(healthScore, width, height, borderWidth, showText) {
+  var idx = scores;
   scores++;
 
-
-  var cvs = canvases[score] = document.createElement('canvas');
+  var cvs = canvases[idx] = document.createElement('canvas');
   cvs.width = width;
   cvs.height = height;
-  healths[score] = healthScore;
-  dispHealths[score] = 0;
-  window.requestAnimationFrame(function() {draw(score);});
+  healths[idx] = healthScore;
+  dispHealths[idx] = 0;
+  borderWidths[idx] = borderWidth;
+  showTexts[idx] = showText;
+  window.requestAnimationFrame(function() {draw(idx);});
   return cvs;
 }
 
-function draw(score) {
-  var ctx = canvases[score].getContext('2d');
+function draw(idx) {
+  var ctx = canvases[idx].getContext('2d');
   ctx.imageSmoothingEnabled = true;
-  var w = canvases[score].width;
-  var h = canvases[score].height;
+  var w = canvases[idx].width;
+  var h = canvases[idx].height;
   ctx.clearRect(0, 0, w, h);
 
   if(w > h)
-    drawHoriziontal(ctx, w, h, dispHealths[score]);
+    drawHoriziontal(ctx, w, h, dispHealths[idx], borderWidths[idx], showTexts[idx]);
   else
-    drawVertical(ctx, w, h, dispHealths[score]);
+    drawVertical(ctx, w, h, dispHealths[idx], borderWidths[idx], showTexts[idx]);
 
-  if(dispHealths[score] != healths[score]) {
-    dispHealths[score] += (healths[score] - dispHealths[score]) * rate;
-    if(Math.abs(healths[score] - dispHealths[score]) < .005)
-      dispHealths[score] = healths[score];
-    window.requestAnimationFrame(function() {draw(score);});
+  if(dispHealths[idx] != healths[idx]) {
+    dispHealths[idx] += (healths[idx] - dispHealths[idx]) * rate;
+    if(Math.abs(healths[idx] - dispHealths[idx]) < .005)
+      dispHealths[idx] = healths[idx];
+    window.requestAnimationFrame(function() {draw(idx);});
   }
 }
 
-function drawHoriziontal(ctx, w, h, health) {
+function drawHoriziontal(ctx, w, h, health, borderWidth, showText) {
   ctx.fillStyle = glassColor;
   ctx.beginPath();
   ctx.arc(h/2, h/2, h/2, 0, 2*Math.PI);
@@ -62,14 +64,16 @@ function drawHoriziontal(ctx, w, h, health) {
 
   ctx.roundRect(h/2, h/4, (w-(h/2)-borderWidth)*health, h/2, borderRadius).fill();
 
-  ctx.fillStyle = glassColor;
-  ctx.font = Math.round(Math.min(w, h)/3)+'px '+fontName;
-  var pct = Math.round(health * 100)+"%";
-  //yes, I'm using the w of the letter 'M' to approximate the height of a string... it seems to work
-  ctx.fillText(pct, (h/2) - (ctx.measureText(pct).width/2), h/2 + ctx.measureText('M').width/2);
+  if(showText) {
+    ctx.fillStyle = glassColor;
+    ctx.font = Math.round(Math.min(w, h)/3)+'px '+fontName;
+    var pct = Math.round(health * 100)+"%";
+    //yes, I'm using the w of the letter 'M' to approximate the height of a string... it seems to work
+    ctx.fillText(pct, (h/2) - (ctx.measureText(pct).width/2), h/2 + ctx.measureText('M').width/2);
+  }
 }
 
-function drawVertical(ctx, w, h, health) {
+function drawVertical(ctx, w, h, health, borderWidth, showText) {
   ctx.fillStyle = glassColor;
   ctx.beginPath();
   ctx.arc(w/2, h-w/2, w/2, 0, 2*Math.PI);
@@ -85,10 +89,12 @@ function drawVertical(ctx, w, h, health) {
 
   ctx.roundRect(w/4, borderWidth+(h-(w/2))*(1-health), w/2, h-w/2-(h-(w/2))*(1-health), borderRadius).fill();
 
-  ctx.fillStyle = glassColor;
-  ctx.font = Math.round(Math.min(w, h)/3)+'px '+fontName;
-  var pct = Math.round(health * 100)+"%";
-  ctx.fillText(pct, (w/2) - (ctx.measureText(pct).width/2), h - w/2 + ctx.measureText('M').width/2);
+  if(showText) {
+    ctx.fillStyle = glassColor;
+    ctx.font = Math.round(Math.min(w, h)/3)+'px '+fontName;
+    var pct = Math.round(health * 100)+"%";
+    ctx.fillText(pct, (w/2) - (ctx.measureText(pct).width/2), h - w/2 + ctx.measureText('M').width/2);
+  }
 }
 
 
