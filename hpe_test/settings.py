bc38eeb97a3b2d06da165f063d9773411ae37cbf
Caleb Kussmaul <cwkussmaul@wpi.edu>
2017-05-06 12:44:46 -0400

    Added 127.0.0.1 to allowed hosts

--- hpe_test/settings.py
+++ hpe_test/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['nshenoy.com:8080','nshenoy.com','nshenoy.com:8000','localhost']
+ALLOWED_HOSTS = ['nshenoy.com:8080','nshenoy.com','nshenoy.com:8000','localhost', '127.0.0.1']
 
 
 # Application definition
